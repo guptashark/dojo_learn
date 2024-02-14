e8b027e6282de3b51a498b8679d852087b4cdedc
Aishwary Gupta <guptashark@protonmail.com>
2024-02-14 09:59:02 -0500

    Use local dojo src.

--- dojo_config.js
+++ dojo_config.js
@@ -3,8 +3,12 @@ var dojoConfig = {
   async: true,
   cacheBust: new Date(),
   waitSeconds: 5,
+  baseUrl: '.',
 
   packages: [
+    'dojo',
+    'dijit',
+    'dojox',
     {
       name: "demo",
       location: location.pathname.replace(/\/[^/]*$/, '') + '/demo'
